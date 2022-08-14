d72b04342458464aa0952c11baba5c48d9669bdc
Praveen Kumar <praveenkumarthipparam@gmail.com>
2022-08-13 22:24:31 -0500

    Initial Commit

---
+++ src/Navbar.js
@@ -0,0 +1,69 @@
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars } from "react-icons/fa";
+import { links, social } from "./data";
+import logo from "./logo.svg";
+
+const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
+
+  return (
+    <nav>
+      {/* NavHeader */}
+      <div className="nav-center">
+        <div className="nav-header">
+          <img className="logo" src={logo} alt="logo" />
+          <button
+            type="button"
+            className="nav-toggle"
+            onClick={() => {
+              setShowLinks(!showLinks);
+            }}
+          >
+            <FaBars></FaBars>
+          </button>
+        </div>
+
+        {/* Nav-Links */}
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+
+        {/* Social Media Links   */}
+        <div>
+          <ul className="social-icons">
+            {social.map((socialIcon) => {
+              const { id, url, icon } = socialIcon;
+              return (
+                <li key={id}>
+                  <a href={url}>{icon}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
